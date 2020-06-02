38dc71f73bcba54a20e17ca7af260f4a1d46bffd
Vera Scheunert <vera.scheunert@aol.com>
2020-06-02 16:48:45 +0200

    Add ChallengeCard component with axios calls on mock-data

---
+++ src/components/ChallengeCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>{{ challenge.title }}</h2>
+    <p>{{ challenge.points }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["challenge"]
+};
+</script>
+
+<style scoped></style>
