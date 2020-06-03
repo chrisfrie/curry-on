42a93d515847bb84d6d84555ab45423800d32a0f
Gabi Franz <gabrielafranz85@gmail.com>
2020-06-03 14:49:35 +0200

    Add content on about page and butler image

---
+++ src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <div>
+      <img src="public/butler.jpg" alt="Butler" />
+      <p>
+        Manfred, a valuable butler from the 50s, worked all his life serving
+        oysters in the hippiest restaurant in Berlin.
+      </p>
+      <p>
+        However, he always dreamed of currywurst. He loved currywurst and
+        preferred to prepare and eat it in any version. Nowadays his ghost
+        always challenge currywurst lovers to be served by him, but is not only
+        eat the wurst and enjoy the curry... is something else. Help Manfred and
+        make his dream come true.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
