8c452b362bcaf33aa2a7cd8ce790df8ed9cea817
Gabi Franz <gabrielafranz85@gmail.com>
2020-06-03 12:14:39 +0200

    Add setup frame of about page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "dashboard",
     component: Dashboard
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: About
   }
 ];
 
