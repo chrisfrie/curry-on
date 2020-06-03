03af93ca6ec049353244740952c57aa179c069fe
Gabi Franz <gabrielafranz85@gmail.com>
2020-06-03 18:16:19 +0200

    Add more styling to about.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,11 +2,14 @@
   <div>
     <h1>About</h1>
     <div class="about-card">
-      <img src="../../public/butler.jpg" alt="Butler" />
+      <div class="about-card-intro">
+      <!--<img src="../../public/butler.jpg" alt="Butler" /> --->
+       <img src="../../public/butler.png" alt="Butler" />
       <p>
         Manfred, a valuable butler from the 50s, worked all his life serving
         oysters in the hippiest restaurant in Berlin.
       </p>
+      </div>
       <p>
         However, he always dreamed of currywurst. He loved currywurst and
         preferred to prepare and eat it in any version. Nowadays his ghost
@@ -24,11 +27,22 @@ export default {};
 
 <style scoped>
 .about-card {
-  background-color: 545454;
-  display: grid;
-  grid-template-columns: 100px 200px;
-  grid-template-rows: 100px 100px;
+  background-color: #545454;
+  color: #EBF1F0;
+  border-radius: 20px;
+  box-sizing:border-box;
+  padding: 0 20px 20px 10px;
+  text-align: center;
+  
+}
+
+.about-card-intro {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
   justify-content: space-around;
 }
+
+
 
 </style>
