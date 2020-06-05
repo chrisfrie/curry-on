6face10ae971e9c6715ef8644a948fd48cc4423d
Chaz Gagne <chazgagne@gmail.com>
2020-06-05 14:16:44 +0200

    added example Register.vue script

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -10,10 +10,25 @@
         Upload Avatar
         <input @change="fileSelected" type="file"
       /></label> -->
-      <input type="text" name="username" placeholder="Username" />
-      <input type="email" name="email" placeholder="Email" />
+      <input
+        v-model="users.name"
+        type="text"
+        name="name"
+        placeholder="Username"
+      />
+      <input
+        v-model="users.email"
+        type="email"
+        name="email"
+        placeholder="Email"
+      />
 
-      <input type="password" name="password" placeholder="Password" />
+      <input
+        v-model="users.password"
+        type="password"
+        name="password"
+        placeholder="Password"
+      />
 
       <button type="submit">Register</button>
     </form>
@@ -21,7 +36,49 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      users: {
+        username: "",
+        email: "",
+        password: ""
+      },
+      avatar: null
+    };
+  },
+  computed: {
+    avatarURL() {
+      return this.avatar ? window.URL.createObjectURL(this.avatar) : null;
+    }
+  },
+  methods: {
+    fileSelected(event) {
+      // Check if a file was selected
+      if (event.target.files.length == 0) {
+        return;
+      }
+
+      // Set avatar to selected file
+      this.avatar = event.target.files[0];
+    },
+    async submit() {
+      try {
+        await this.$store.dispatch("register", this.user);
+        this.$router.push("/");
+
+        if (this.avatar) {
+          await this.$store.dispatch("createProfile", this.avatar);
+        }
+      } catch {
+        this.$store.dispatch("pushNotification", {
+          type: "error",
+          message: "Sorry, couldn't register, please check your inputs"
+        });
+      }
+    }
+  }
+};
 </script>
 
 <style scoped></style>
