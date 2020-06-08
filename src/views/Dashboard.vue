877c8151bbb4cdba6e3884b8a89c61580cf51ed0
Gabi Franz <gabrielafranz85@gmail.com>
2020-06-08 14:52:08 +0200

    Create chapter components and route them on dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h1>Curry On!</h1>
+    <h1>Manfred's Tale!</h1>
+    <Chapter1 />
+    <Chapter2 />
+    <Chapter3>
+      <Chatbot />
+    </Chapter3>
     <div v-for="(challenges, index) in chapters" :key="index" class="chapter">
       <ChallengeCard
         v-for="challenge in challenges"
@@ -8,17 +13,22 @@
         :challenge="challenge"
       />
     </div>
-    <Chatbot />
   </div>
 </template>
 
 <script>
+import Chapter1 from "@/components/Chapter1.vue";
+import Chapter2 from "@/components/Chapter2.vue";
+import Chapter3 from "@/components/Chapter3.vue";
 import Chatbot from "@/components/Chatbot.vue";
 import ChallengeCard from "@/components/ChallengeCard.vue";
 import { getChallenges } from "@/services/challenge-service.js";
 
 export default {
   components: {
+    Chapter1,
+    Chapter2,
+    Chapter3,
     ChallengeCard,
     Chatbot
   },
