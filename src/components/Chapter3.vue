9b4ee0ac402d4e3546915939e86d38e833693268
Gabi Franz <gabrielafranz85@gmail.com>
2020-06-08 15:12:53 +0200

    Add template and script to Chapter.vue

---
+++ src/components/Chapter3.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+      <h2>Chapter Three</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
+        consectetur animi quis dolore, voluptate dolores. Excepturi consectetur
+        laudantium dolorem magni quo natus sit placeat error delectus veritatis,
+        cupiditate, quaerat recusandae.
+      </p>
+    </div>
+    <div v-for="(challenges, index) in chapters" :key="index" class="chapter">
+      <ChallengeCard
+        v-for="challenge in challenges"
+        :key="challenge.id"
+        :challenge="challenge"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import ChallengeCard from "@/components/challengecard.vue";
+
+export default {
+  components: {
+    ChallengeCard
+  }
+};
+</script>
+
+<style scoped></style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,13 +6,6 @@
     <Chapter3>
       <Chatbot />
     </Chapter3>
-    <div v-for="(challenges, index) in chapters" :key="index" class="chapter">
-      <ChallengeCard
-        v-for="challenge in challenges"
-        :key="challenge.id"
-        :challenge="challenge"
-      />
-    </div>
   </div>
 </template>
 
@@ -21,37 +14,15 @@ import Chapter1 from "@/components/Chapter1.vue";
 import Chapter2 from "@/components/Chapter2.vue";
 import Chapter3 from "@/components/Chapter3.vue";
 import Chatbot from "@/components/Chatbot.vue";
-import ChallengeCard from "@/components/ChallengeCard.vue";
-import { getChallenges } from "@/services/challenge-service.js";
 
 export default {
   components: {
     Chapter1,
     Chapter2,
     Chapter3,
-    ChallengeCard,
     Chatbot
   },
-  data() {
-    return {
-      challenges: []
-    };
-  },
-  async created() {
-    this.challenges = (await getChallenges()).data;
-  },
-  computed: {
-    chapters() {
-      const result = [];
-      for (let chapterId = 1; chapterId <= 3; chapterId++) {
-        const chapter = this.challenges.filter(
-          challenge => challenge.chapterId == chapterId
-        );
-        result.push(chapter);
-      }
-      return result;
-    }
-  }
+
 };
 </script>
 
