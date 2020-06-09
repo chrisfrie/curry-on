ad4fe41103af9532156881a3d7946d43ad7478f1
Chaz Gagne <chazgagne@gmail.com>
2020-06-09 11:54:34 +0200

    shows avatar on profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,9 +2,11 @@
   <div>
     <h2>Profile</h2>
     <section class="profile-body">
-      <div class="avatar"><img src="imageURL" /></div>
-      <h2>{{ profile.user.username }}</h2>
-      <h3>{{ profile.pommesPoints }}</h3>
+      <div class="avatar"><img :src="imageURL" /></div>
+      <div class="user-info">
+        <h2>UserName: {{ profile.user.username }}</h2>
+        <p>Current Pommes Points: {{ profile.pommesPoints }}</p>
+      </div>
     </section>
   </div>
 </template>
@@ -13,11 +15,28 @@
 export default {
   props: ["profile"],
   computed: {
-    imageUrl() {
-      return process.env.VUE_APP_UPLOAD_URL + this.profile.avatar.url;
+    imageURL() {
+      return "http://localhost:3000" + this.profile.avatar.url;
     }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.profile-body {
+  display: flex;
+  justify-content: center;
+}
+.avatar img {
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  border: 1px black;
+  border-radius: 50%;
+  margin-right: 2rem;
+}
+.user-info {
+  text-align: left;
+  align-self: center;
+}
+</style>
