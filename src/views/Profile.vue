06796d17882784098fa13fcb9af26cee267971a9
Felix Wippich <felix.wippich@me.com>
2020-06-10 17:07:25 +0200

    Try PommesPoints binding on the Profile.vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,8 +5,7 @@
       <div class="avatar"><img :src="imageURL" /></div>
       <div class="user-info">
         <h2>{{ profile.user.username }}</h2>
-        <p>Current Pommes Points: {{ profile.pommesPoints }}</p>
-        <p>{{ $store.state.user.id }}</p>
+        <p>Current Pommes Points: {{ pommesPoints }}</p>
       </div>
     </section>
     <ProfileGallery :picture="$store.state.user.pictures" />
@@ -24,6 +23,21 @@ export default {
   computed: {
     imageURL() {
       return "http://localhost:1337" + this.profile.avatar.url;
+    },
+    pommesPoints() {
+      let currentPoints = 0;
+      let pictures = [this.$store.state.user.pictures];
+      for (const element of pictures) {
+        if (element.find(element => element.challenge == 1)) {
+          currentPoints += this.$store.state.challenges.pommesPoints;
+        } else if (element.find(element => element.challenge == 2)) {
+          currentPoints += 6;
+        } else if (element.find(element => element.challenge == 3)) {
+          currentPoints += 6;
+        }
+      }
+
+      return currentPoints;
     }
   }
 };
