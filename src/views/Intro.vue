a8f9917ec678d4a68060c2be3fef424fab82d5bb
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-06-11 15:47:01 +0200

    Fix styling of register

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -70,18 +70,15 @@ export default {
 
 <style scoped>
 #loginPopUp {
-  top: 50%;
-  left: 50%;
-  height: 300px;
-  width: 300px;
-  margin-top: -150px;
-  margin-left: -150px;
-  opacity: 0.95;
+  width: 100vw;
+  height: 100vh;
   position: fixed;
-  background-color: #313131;
-  overflow: auto;
-  border-radius: 1%;
-  color: var(--light-text);
+  top: 0;
+  left: 0;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .about-card {
